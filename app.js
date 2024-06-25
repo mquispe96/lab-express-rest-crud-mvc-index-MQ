39037c14dd160719d6870fa28055c517a9356421
mquispe96 <mquispe@pursuit.org>
2024-06-25 12:26:31 -0400

    Fix route controllers

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello, world!'));
 app.use('/locations', locationsController);
-app.use('/machines', peopleController);
-app.use('/persons', plansController);
-app.use('/plans', machinesController);
+app.use('/machines', machinesController);
+app.use('/persons', peopleController);
+app.use('/plans', plansController);
 app.use('/special-events', eventsController);
 app.use('*', (req, res) => res.status(404).send({error: 'Sorry, no page found!'}));
 
